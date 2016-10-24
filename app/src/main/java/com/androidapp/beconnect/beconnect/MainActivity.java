eae9d8c0cc6e1de3fd42cad4bdc297896757d5a8
mitour <love2082020@gmail.com>
2016-10-24 16:19:58 +0800

    Set Login link

--- app/src/main/java/com/androidapp/beconnect/beconnect/MainActivity.java
+++ app/src/main/java/com/androidapp/beconnect/beconnect/MainActivity.java
@@ -1,13 +1,28 @@
 package com.androidapp.beconnect.beconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bLogin = (Button) findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(LoginIntent);
+            }
+        });
     }
 }
