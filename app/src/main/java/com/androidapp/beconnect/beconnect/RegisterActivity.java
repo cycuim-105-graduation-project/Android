cbfa57393f41a45ddb28977888b77f8d420ea853
mitour <love2082020@gmail.com>
2016-10-26 14:12:07 +0800

    Interfacing register API

--- app/src/main/java/com/androidapp/beconnect/beconnect/RegisterActivity.java
+++ app/src/main/java/com/androidapp/beconnect/beconnect/RegisterActivity.java
@@ -9,7 +9,16 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.androidapp.beconnect.beconnect.app.AppController;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,17 +28,22 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText etEmail;
     private EditText etPassword;
     private EditText etPassword_confirmation;
-    private Button   bRegister;
+    private Button bRegister;
     private TextView tvLoginLink;
 
     private static final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
     private Matcher matcher;
 
+    String url;
+    String tag_string_req = "string_req";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        url = getResources().getString(R.string.url_register);
 
         etIdentity               = (EditText) findViewById(R.id.etIdentity);
         etEmail                  = (EditText) findViewById(R.id.etEmail);
@@ -66,6 +80,32 @@ public class RegisterActivity extends AppCompatActivity {
                     etEmail.setError(null);
                     etPassword.setError(null);
                     etPassword_confirmation.setError(null);
+
+                    StringRequest sr = new StringRequest(Request.Method.POST, url , new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Toast.makeText(RegisterActivity.this, "please check your mailbox to confirm this account", Toast.LENGTH_LONG).show();
+                            Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            RegisterActivity.this.startActivity(loginIntent);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }){
+
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("identity",              identity);
+                            params.put("email",                 email);
+                            params.put("password",              password);
+                            params.put("password_confirmation", password_confirmation);
+                            return params;
+                        }
+                    };
+                    AppController.getInstance().addToRequestQueue(sr, tag_string_req);
                 }
             }
         });
