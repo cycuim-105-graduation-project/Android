371fcd993af1c8df847d49ac6b39134a28c42736
mitour <love2082020@gmail.com>
2016-10-26 14:28:24 +0800

    interfacing validate Token API (EditUserProfileActivity)

--- app/src/main/java/com/androidapp/beconnect/beconnect/EditUserProfile.java
+++ app/src/main/java/com/androidapp/beconnect/beconnect/EditUserProfile.java
@@ -1,13 +1,110 @@
 package com.androidapp.beconnect.beconnect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.androidapp.beconnect.beconnect.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditUserProfile extends AppCompatActivity {
+
+    private TextView tvIdentity;
+    private TextView tvEmail;
+    private EditText etNickname;
+    private EditText etFirstname;
+    private EditText etLastname;
+    private EditText etCellphone;
+    private EditText etZipcode;
+    private EditText etAddress;
+    private EditText etCompany;
+    private EditText etCompanyAddress;
+    private EditText etJobTitle;
+    private Button   bEdit;
+
+    SessionManager session;
+
+    String url_validate_token;
+    String tag_string_req = "string_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_profile);
+
+        tvIdentity       = (TextView) findViewById(R.id.tvIdentity);
+        tvEmail          = (TextView) findViewById(R.id.tvEmail);
+        etNickname       = (EditText) findViewById(R.id.etNickname);
+        etLastname       = (EditText) findViewById(R.id.etLastname);
+        etFirstname      = (EditText) findViewById(R.id.etFirstname);
+        etCellphone      = (EditText) findViewById(R.id.etCellphone);
+        etZipcode        = (EditText) findViewById(R.id.etZipcode);
+        etAddress        = (EditText) findViewById(R.id.etAddress);
+        etCompany        = (EditText) findViewById(R.id.etCompany);
+        etCompanyAddress = (EditText) findViewById(R.id.etCompanyAddress);
+        etJobTitle       = (EditText) findViewById(R.id.etJobTitle);
+        bEdit            = (Button)   findViewById(R.id.bEdit);
+
+        url_validate_token    = getResources().getString(R.string.url_validate_token);
+
+        session = new SessionManager(getApplicationContext());
+
+        session.checkLogin();
+
+        HashMap<String, String> user = session.getUserDetails();
+
+        final String uid          = user.get(SessionManager.KEY_UID);
+        final String access_token = user.get(SessionManager.KEY_ACCESS_TOKEN);
+        final String client       = user.get(SessionManager.KEY_CLIENT);
+        final String content_type = user.get(SessionManager.KEY_CONTENT_TYPE);
+
+        JsonObjectRequest jor = new JsonObjectRequest(url_validate_token, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                          tvIdentity.setText(response.getJSONObject("data").getString("identity"));
+                             tvEmail.setText(response.getJSONObject("data").getString("email"));
+                          etNickname.setText(response.getJSONObject("data").getString("nickname"));
+                         etFirstname.setText(response.getJSONObject("data").getString("firstname"));
+                          etLastname.setText(response.getJSONObject("data").getString("lastname"));
+                         etCellphone.setText(response.getJSONObject("data").getString("cellphone"));
+                           etZipcode.setText(response.getJSONObject("data").getString("zipcode"));
+                           etAddress.setText(response.getJSONObject("data").getString("address"));
+                           etCompany.setText(response.getJSONObject("data").getString("company"));
+                    etCompanyAddress.setText(response.getJSONObject("data").getString("company_address"));
+                          etJobTitle.setText(response.getJSONObject("data").getString("job_title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(EditUserProfile.this, "error", Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("access-token", access_token);
+                params.put("client",       client);
+                params.put("uid",          uid);
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(jor, tag_string_req);
+
     }
 }
