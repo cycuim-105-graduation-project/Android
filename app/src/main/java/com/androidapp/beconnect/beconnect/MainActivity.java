bcc33419bb238de674a96322339c405ea4757d3c
mitour <love2082020@gmail.com>
2016-12-01 13:38:22 +0800

    user permission check (for Android 6.0 or newer)

--- app/src/main/java/com/androidapp/beconnect/beconnect/MainActivity.java
+++ app/src/main/java/com/androidapp/beconnect/beconnect/MainActivity.java
@@ -1,11 +1,17 @@
 package com.androidapp.beconnect.beconnect;
 
+import android.Manifest;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
 
     Button bLogin;
     private MonitorService mService = null;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,23 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             setTitle("Bind failed! Manifest?");
         }
 
+        // 要使用者權限 (for Android 6.0 後)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check 
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
     }
 
 
@@ -80,5 +105,30 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         // Activity is not in foreground, make a trade-off between battery usage and scan latency
         OneBeacon.setScanStrategy(ScanStrategy.BALANCED);
         super.onPause();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 }
