147df138f2d786972888d37df4f06031c39fedaa
mitour <love2082020@gmail.com>
2016-12-07 19:40:42 +0800

    set Beacon monitor connect in index page (event activity)

--- app/src/main/java/com/androidapp/beconnect/beconnect/Events.java
+++ app/src/main/java/com/androidapp/beconnect/beconnect/Events.java
@@ -1,8 +1,11 @@
 package com.androidapp.beconnect.beconnect;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,13 +14,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Events extends AppCompatActivity {
+import io.onebeacon.api.OneBeacon;
+import io.onebeacon.api.ScanStrategy;
+
+public class Events extends AppCompatActivity implements ServiceConnection {
 
     private BluetoothAdapter mBluetoothAdapter;
     private static final int REQUEST_ENABLE_BT = 1;
 
     Button bEventDetail1;
     Button bEventDetail2;
+
+    private MonitorService mService = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,10 @@ public class Events extends AppCompatActivity {
             }
         }
 
+        if (!bindService(new Intent(this, MonitorService.class), (ServiceConnection) this, BIND_AUTO_CREATE)) {
+            setTitle("Bind failed! Manifest?");
+        }
+
     }
 
     // 使用onActivityResult 接收其他 Activity回傳的資料
@@ -76,7 +88,6 @@ public class Events extends AppCompatActivity {
             return;
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -113,5 +124,48 @@ public class Events extends AppCompatActivity {
 
         return true;
     }
+    @Override
+    protected void onDestroy() {
+        // Activity is gone, set scan mode to use lowest possible power usage
+        OneBeacon.setScanStrategy(ScanStrategy.LOW_POWER);
+        if (null != mService) {
+            // optionally stop the service if running in background is not desired
+//            stopService(new Intent(this, MonitorService.class));
+            unbindService(this);
+            mService = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        mService = ((MonitorService.LocalServiceBinder) service).getService();
+        setTitle("Service connected");
+
+        // make the service to stick around by actually starting it
+        startService(new Intent(this, MonitorService.class));
+
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        mService = null;
+        setTitle("Service disconnected");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Activity is visible, scan with most reliable results
+        OneBeacon.setScanStrategy(ScanStrategy.LOW_LATENCY);
+    }
+
+    @Override
+    protected void onPause() {
+        // Activity is not in foreground, make a trade-off between battery usage and scan latency
+        OneBeacon.setScanStrategy(ScanStrategy.BALANCED);
+        super.onPause();
+    }
+
 }
 
